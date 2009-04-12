e8cde072b5c3342e88f2167571fdd7f93b83b296
snusnu <gamsnjaga@gmail.com>
2009-04-12 04:33:57 +0200

    readability

--- lib/ken/util.rb
+++ lib/ken/util.rb
@@ -2,11 +2,14 @@ module Ken
   module Util
     # magic hash conversion
     def convert_hash(source)
-      result = {}
-      source.each do |item|
-        result[item["link"]] ? result[item["link"]] << { "id" => item["id"], "name" => item["name"], "value" => item["value"] } : result[item["link"]] = [] << { "id" => item["id"], "name" => item["name"], "value" => item["value"] }
+      source.inject({}) do |result, item|
+        if result[item["link"]]
+          result[item["link"]] << { "id" => item["id"], "name" => item["name"], "value" => item["value"] }
+        else
+          result[item["link"]] = []
+          result[item["link"]] << { "id" => item["id"], "name" => item["name"], "value" => item["value"] }
+        end
       end
-      result
     end
     module_function :convert_hash
   end
