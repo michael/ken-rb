8adacc0cf8ca94146bb12c0524ef26093d9b6a4d
michael <ma@zive.at>
2009-08-24 23:29:31 +0200

    use cursored query in artist_links example

--- examples/artist_links.rb
+++ examples/artist_links.rb
@@ -9,20 +9,30 @@ require EXAMPLES_ROOT.parent + 'lib/ken'
 
 Ken::Session.new('http://www.freebase.com', 'ma', '*****')
 
+puts "collecting artist links... this might take a while..."
+
+artist_links = []
+
+# execute query
 artists = Ken.session.mqlread([{
   :type => "/music/artist",
   :id => nil, 
   :"/common/topic/webpage" => [{:uri => nil}], 
-  :home_page => [{:uri => nil}], 
-  :limit => 50
-}])
+  :home_page => [{:uri => nil}]
+}], :cursor => true)
 
+# collect artist links
 artists.each do |artist|
   artist["/common/topic/webpage"].each do |webpage|
-    puts webpage["uri"]
+    artist_links << webpage["uri"] unless artist_links.include?(webpage["uri"])
   end
   
   artist["home_page"].each do |homepage|
-    puts homepage["uri"]
+    artist_links << homepage["uri"] unless artist_links.include?(homepage["uri"])
   end
+end
+
+# print artist links
+artist_links.each do |link|
+  puts link
 end
